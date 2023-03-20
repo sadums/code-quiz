b37a80cc1523a5935fbd56d9ed9170b165056423
sadums <samjadams4@gmail.com>
2023-03-20 17:49:22 -0600

    cleaned/organized javascript and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,179 +1,162 @@
-var main = $('main');
-if(localStorage.getItem("highScore") === undefined){
-    localStorage.setItem("highScore", JSON.stringify([]));
-}
-var difficulties = ['Easy', 'Medium', 'Hard'];
-
-
-// Question/Answer arrays
-
-var questionSheets = [questionSheetEasy, questionSheetMedium, questionSheetHard];
-
-// Initialize radio buttons
-
-var difficultyRadioBtns = document.getElementsByName('difficulty');
-difficultyRadioBtns[0].checked = true;
-var selectedDifficulty = 0;
-var quizTitleEl = $('#quizTitle');
-var selectedQuestionSheet = questionSheets[selectedDifficulty];
-
-var checkButtons = function(){
-    for(let i = 0; i < difficultyRadioBtns.length; i++){
-        if(difficultyRadioBtns[i].checked == true){
-            selectedDifficulty = i;
-        }
-    }
-    selectedQuestionSheet = questionSheets[selectedDifficulty];
-    quizTitleEl.text(`Code Quiz: ${difficulties[selectedDifficulty]}`);
-}
-
-checkButtons();
-difficultyRadioBtns.forEach( (x) => x.addEventListener("click", checkButtons));
-
-// Start
-
-var startCard = $('#start');
-var startButton = $('#startButton');
-
+// Game Variables
 var score;
 var time;
+var questionNum;
+var questionObject;
+var questionSheet;
+
+// Question/Answer arrays
+var difficulties = ['Easy', 'Medium', 'Hard'];
+var questionSheets = [questionSheetEasy, questionSheetMedium, questionSheetHard];
+
+
+
+/* HTML ELEMENTS */
+
+// Nav Bar Elements
+var homeTabLink = $("#homeTab");
+var highScoresTabLink = $("#highScoresTab");
+
+// Home Screen
+var main = $('#main');
+
+// Start Screen Elements
+var startScreen = $('#start');
+var quizTitleEl = $('#quizTitle');
+var startButton = $('#startButton');
+
+// Game Screen Elements
+var gameScreen = $('#game');
+var scoreEl = $('#score');
+var timerEl = $('#timer');
+var questionNumEl = $('#questionNum');
+var questionEl = $('#question');
+var buttonEls = [$('#0'), $('#1'), $('#2'), $('#3')];
+
+// Game End Screen Elements
+var gameEndScreen = $('#finish');
+var timeLeftEl = $('#timeLeft');
+var finalScoreEl = $('#finalScore');
+var submitEl = $('#submit');
+var restartEl = $('#restart');
+var initialsInput = $('#initials');
+
+// High Score Screen Elements
+var highScoresTab = $("#highScores");
+var highScoreList = $("#highScoresList");
+var backButton = $('#back');
+
+
+
+/* TAB SWITCH FUNCTIONS */
+var hideTabs = function(){
+    startScreen.css('display', 'none');
+    gameScreen.css('display', 'none');
+    gameEndScreen.css('display', 'none');
+    highScoresTab.css('display', 'none');
+}
+
+var showStartTab = function(){
+    hideTabs();
+    main.css('display', 'block');
+    startScreen.css('display', 'block');
+}
+
+var showGameTab = function(){
+    hideTabs();
+    main.css('display', 'block');
+    gameScreen.css('display', 'block');
+}
+
+var showGameEndTab = function(){
+    hideTabs();
+    main.css('display', 'block');
+    gameEndScreen.css('display', 'block');
+}
+
+var showHighScoreTab = function(){
+    homeTabLink.attr("class", "nav-link");
+    highScoresTabLink.attr("class", "nav-link active");
+    main.css('display', 'none');
+    highScoresTab.css('display', 'block');
+    updateHighScores();
+}
+
+
+
+/* GAME FUNCTIONS */
+var updateTime = function(){
+    timerEl.text("Time: " + time.toFixed(1));
+}
+var updateScore = function(){
+    scoreEl.text('Score: ' + score.toFixed(0));
+}
+
+var startTimer = function(){
+    var timerInterval = setInterval(function() {
+        if(time <= 0){
+            time = 0;
+            updateTime();
+            clearInterval(timerInterval);
+            endGame();
+        }else{
+            updateTime();
+        }
+        time-= 0.1;
+    }, 100);
+}
 
 var start = function(){
     time = 60;
     score = 0;
-    startCard.css('display','none');
-    main.html(`
-    <div id="start" class="row align-items-md-stretch align-items-center justify-content-center">
-        <div class="col-md-6">
-            <div class="h-100 p-5 text-white bg-dark border rounded-3 shadow">
-            <div class="row">
-                <h4 class="col-sm-8" id="score">Score: 0</h4>
-                <h4 class="col-sm-4" id="timer">Time: </h4>
-            </div>
-                <div class="h-100 p-5 text-white bg-dark border rounded-3 shadow">
-                <h2 id="questionNum">Question #</h2>
-                <p id="question"></p>
-                <div class="container vstack gap-3">
-                    <button id="0" class="btn btn-outline-light text-blue grow d-flex justify-content-start align-items-center" type="button"></button>
-                    <button id="1" class="btn btn-outline-light text-blue grow d-flex justify-content-start align-items-center" type="button"></button>
-                    <button id="2" class="btn btn-outline-light text-blue grow d-flex justify-content-start align-items-center" type="button"></button>
-                    <button id="3" class="btn btn-outline-light text-blue grow d-flex justify-content-start align-items-center" type="button"></button>
-                </div>
-            </div>
-        </div>
-    </div>
-    `);
+    questionNum = 0;
+    questionSheet = selectedQuestionSheet;
+
+    updateScore();
+    updateTime();
+    showGameTab();
     startTimer();
-}
-
-startButton.on('click', start);
-
-var startTimer = function(){
-    var timerEl = $('#timer');
-    var timerInterval = setInterval(function() {
-        if(time <= 0){
-            timerEl.text("Time: 0");
-            time = 0;
-            clearInterval(timerInterval);
-            endGame();
-        }else{
-            timerEl.text("Time: " + time.toFixed(1));
-        }
-        time-= 0.1;
-    }, 100);
-    startGame();
-}
-
-var startGame = function(){
-    var scoreEl = $('#score');
-    var questionNumEl = $('#questionNum');
-    var questionEl = $('#question');
-    var buttonEls = [$('#0'), $('#1'), $('#2'), $('#3')];
-    var questionNum = 0;
-    var questionObject;
-    var questionSheet = selectedQuestionSheet;
-
-    // Load new question
-    var newQuestion = function(){
-        if(questionNum >= 10){
-            endGame();
-            return;
-        }
-        questionNumEl.text("Question #" + (questionNum + 1));
-
-        questionObject = questionSheet[questionNum];
-        questionEl.text(questionObject.question);
-        for(var i = 0; i < buttonEls.length; i++){
-            buttonEls[i].text(questionObject.answers[i]);
-        }
-        questionNum++;
-    }
-
-    var correct = function(){
-        score += time * 100
-        scoreEl.text('Score: ' + score.toFixed(0));
-    }
-
-    var incorrect = function(){
-        time -= 10;
-    }
-
-    // Question answered function
-    buttonEls.forEach( (x) => x.on("click", function(e){
-        if(+x.attr("id") === questionObject.correct){
-            correct();
-        }else{
-            incorrect();
-        }
-        newQuestion();
-    }));
-
     newQuestion();
 }
 
+var correct = function(){
+    score += time * 100
+    updateScore();
+}
+
+var incorrect = function(){
+    time -= 10;
+}
+
+var newQuestion = function(){
+    if(questionNum >= 10){
+        endGame();
+        return;
+    }
+    questionNumEl.text("Question #" + (questionNum + 1));
+
+    questionObject = questionSheet[questionNum];
+    questionEl.text(questionObject.question);
+    for(var i = 0; i < buttonEls.length; i++){
+        buttonEls[i].text(questionObject.answers[i]);
+    }
+    questionNum++;
+}
+
+
+
+/* GAME END FUNCTIONS */
 var endGame = function(){
-    main.html(
-    `<div id="finish" class="row align-items-md-stretch align-items-center justify-content-center">
-    <div class="col-md-6">
-      <div class="h-100 p-5 text-white bg-dark border rounded-3 shadow">
-        <div class="row">
-          <h3 class="col-sm-8" id="finalScore">Final Score: ${score.toFixed(0)}</h3>
-          <h5 class="col-sm-4" id="timeLeft">Time Left: ${time.toFixed(1)}</h5>
-        </div>
-        <p class="my-3">Enter your initials to go on the leaderboard!</p>
-        <input type="text" name="" id="initials" class="form-control w-25 my-3" placeholder="Initials here" aria-describedby="helpId">
-        <button id="submit" class="btn btn-outline-light text-blue grow mt-2 mr-2" type="button">Submit</button>
-        <button id="restart" class="btn btn-outline-light text-blue grow mt-2 mx-5" type="button">Restart</button>
-      </div>
-    </div>
-  </div>`);
 
-  var submitEl = $('#submit');
-  var restartEl = $('#restart')
-  var initialsInput = $('#initials');
+  showGameEndTab();
 
-
-
-  function restart(){
-    main.html(` 
-    <div id="start" class="row align-items-md-stretch align-items-center justify-content-center">
-    <div class="col-md-6">
-        <div class="h-100 p-5 text-white bg-dark border rounded-3 shadow">
-        <h2 id="quizTitle">Code Quiz: ${difficulties[selectedDifficulty]}</h2>
-        <p class="my-3">Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p>
-        <button id="startButton" class="btn btn-outline-light text-blue grow mt-2" type="button">Start</button>
-        </div>
-    </div>
-    </div>
-    `)
-    quizTitleEl = $('#quizTitle');
-    startCard = $('#start');
-    startButton = $('#startButton');
-    startButton.on('click', start);
-  }
+  timeLeftEl.text('Time Left: ' + time.toFixed(1));
+  finalScoreEl.text('Final Score: ' + score.toFixed(0));
   
+  console.log("submit set")
+}
 
-  submitEl.on("click", function(){
+var submitScore = function(){
     if(+initialsInput.val().length >= 2 && +initialsInput.val().length <= 4){
         var highScoreArray = JSON.parse(localStorage.getItem("highScore"));
         var highScoreObject = {
@@ -181,60 +164,80 @@ var endGame = function(){
             "score": score.toFixed(0),
             "difficulty": difficulties[selectedDifficulty]
         }
-        
-    
         highScoreArray.push(highScoreObject);
         highScoreArray.sort(function(a,b){return +b.score - +a.score});
-        console.log(highScoreArray);
         localStorage.setItem("highScore", JSON.stringify(highScoreArray));
-        restart();
-        switchHighScorePage();
+        showHighScoreTab();
     }else{
-        alert("Please make initials 2 - 4 characters long")
+        alert("Please make initials 2 - 4 characters long");
     }
-  });
-
-  restartEl.on("click", function(){
-    restart();
-  });
 }
 
 
-var homePageLink = $("#homePage");
-var highScoresPageLink = $("#highScoresPage");
-var highScoresPage = $("#highScores")
-var highScoreList = $("#highScoresList")
 
-var switchHighScorePage = function(){
-    main.css("display", "none");
-    highScoresPage.css("display", "block");
-    homePageLink.attr("class", "nav-link");
-    highScoresPageLink.attr("class", "nav-link active");
-
+/* HIGH SCORE FUNCTIONS */ 
+var updateHighScores = function(){
     var highScores = JSON.parse(localStorage.getItem("highScore"));
-    console.log(highScores);
     highScoreList.html('');
-    for(var i = 0; i < highScores.length; i++){
+    if(highScores[0] === undefined){
         var listItem = $('<li>');
-        listItem.text(highScores[i].initials + " - " + highScores[i].score + "  / Difficulty - " + highScores[i].difficulty);
+        listItem.text("No high scores to display!");
         listItem.attr("class", "list-group-item");
         highScoreList.append(listItem);
+    }else{
+        for(var i = 0; i < highScores.length; i++){
+            var listItem = $('<li>');
+            listItem.text(highScores[i].initials + " - " + highScores[i].score + "  / Difficulty - " + highScores[i].difficulty);
+            listItem.attr("class", "list-group-item");
+            highScoreList.append(listItem);
+        }
     }
 }
 
-var backButton = $('#back');
 
 
 
-highScoresPageLink.on("click", switchHighScorePage);
+/* ADD EVENT LISTENERS */ 
+var difficultyRadioButtons = document.getElementsByName('difficulty');
+difficultyRadioButtons[0].checked = true;
+var selectedDifficulty;
 
-var switchHomePage = function(){
-    highScoresPage.css("display", "none");
-    main.css("display", "block");
-    homePageLink.attr("class", "nav-link active");
-    highScoresPageLink.attr("class", "nav-link");
+var checkButtons = function(){
+    for(let i = 0; i < difficultyRadioButtons.length; i++){
+        if(difficultyRadioButtons[i].checked == true){
+            selectedDifficulty = i;
+        }
+    }
+    selectedQuestionSheet = questionSheets[selectedDifficulty];
+    quizTitleEl.text(`Code Quiz: ${difficulties[selectedDifficulty]}`);
 }
+checkButtons();
+difficultyRadioButtons.forEach( (x) => x.addEventListener("click", checkButtons));
 
-backButton.on("click", switchHomePage);
+startButton.on("click", start);
 
-homePageLink.on("click", switchHomePage);
+buttonEls.forEach( (x) => x.on("click", function(e){
+    if(+x.attr("id") === questionObject.correct){
+        correct();
+    }else{
+        incorrect();
+    }
+    newQuestion();
+}));
+
+submitEl.on("click", submitScore);
+
+restartEl.on("click", showStartTab);
+
+highScoresTabLink.on("click", showHighScoreTab);
+
+backButton.on("click", showStartTab);
+
+homeTabLink.on("click", showStartTab);
+
+
+
+// Create array for high scores in local storage, if not already made
+if(localStorage.getItem("highScore") === null || localStorage.getItem("highScore") === undefined){
+    localStorage.setItem("highScore", JSON.stringify([]));
+}
